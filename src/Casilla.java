9b6d9477d6cc89f7bda2899aa0ffa3623a0b60fa
Ariel <PC@TOSHIBA>
2014-09-14 17:29:10 -0600

    casilla ariel

--- src/Casilla.java
+++ src/Casilla.java
@@ -1,30 +1,39 @@
-
 public class Casilla {
-	
-	private boolean empty;
-	
-	private String ptsExtra,Letra;
-	
-	public Casilla(){
-		
-	}
-	
-	public Casilla(boolean empty,String ptsExtra, String Letra){
-		this.empty= empty;
-		this.ptsExtra= ptsExtra;
-		this.Letra=Letra;
-		}
-	
-	public boolean isEmpty(){
-		return empty;
-	}
-	
-	public void setEmpty(boolean valor){
-		empty=valor;
-	}
-	
-	public String getLetra(){
-		return Letra;
+	private String letra;
+	private int valor;
+	private int bonus;
+
+	public Casilla() { // constructor
+		String letra = null;
+		int valor = 0;
+		int bonus = 0;
 	}
 
+	public void setLetra(String letra) { // metodo que le asigana una letra a la
+											// casilla
+		this.letra = letra;
+	}
+
+	public void setValor(int valor) { // metodo que le asigna un valor en puntos
+										// a la casilla
+		this.valor = valor;
+	}
+
+	public void setBonus(String bonus) {// metodo que reasigna un valor en
+										// puntos a la casilla de acuerdo con el
+										// bonus extra que tenga
+		if (bonus == "2L") {
+			this.valor = 2 * this.valor;
+		}
+		if (bonus == "3L") {
+			this.valor = this.valor * 3;
+		}
+		if (bonus == "2P") {
+
+		}
+		if (bonus == "3P") {
+
+		}
+
+	}
 }
