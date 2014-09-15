b44aeef7d2756b2cba6ff74d54fc88e4e44e8d39
fernandohv3927 <cafeherval@gmail.com>
2014-09-15 14:17:20 -0600

    ficha2

---
+++ src/Ficha.java
@@ -0,0 +1,14 @@
+public class Ficha {
+	private int valor;
+	private String letra;
+	ficha(int valor,String letra){
+		this.letra=letra;
+		this.valor=valor;
+	}
+	public String getLetra(){
+		return letra;
+	}
+	public int getValor(){
+		return valor; 
+	} 
+}
